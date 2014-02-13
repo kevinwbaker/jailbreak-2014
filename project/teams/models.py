65b9c69c1cebb0b52f7e51949425a920c97db97a
Kevin Baker <eire32kevin@gmail.com>
2014-02-13 18:27:57 +0000

    Changed teams model, removed paste in CSS

--- project/teams/models.py
+++ project/teams/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
-class University(models.Model):
-	'''University'''
-
-	name = models.CharField(max_length=128)
-	abbreviation = models.CharField(max_length=6)
-
 class Team(models.Model):
-	'''Team taking part'''
+    '''Team taking part'''
 
-	slug = models.SlugField()
-	name = models.CharField(max_length=128)
-	photo = models.FileField(upload_to='teams')
-	sponsor_link = models.URLField()
-	description = models.TextField()
-	university = models.ForeignKey('University')
+    TCD = 'tcd'
+    NUIG = 'nuig'
+    UCC = 'ucc'
+    UCD = 'ucd'
+    UNIVERSITIES = (
+        (TCD, 'TCD'),
+        (NUIG, 'NUIG'),
+        (UCC, 'ucc'),
+        (UCD, 'ucd'),
+    )
+    
+    slug = models.SlugField()
+    name = models.CharField(max_length=128)
+    photo = models.FileField(upload_to='teams')
+    sponsor_link = models.URLField()
+    description = models.TextField()
+    source = models.CharField(max_length=4, choices=UNIVERSITIES)
 
-	long_position = models.DecimalField(max_digits=8, decimal_places=3)
-	lat_position = models.DecimalField(max_digits=8, decimal_places=3)
+    long_position = models.DecimalField(max_digits=8, decimal_places=3)
+    lat_position = models.DecimalField(max_digits=8, decimal_places=3)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
