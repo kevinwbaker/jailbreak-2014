64297e8fc1b1381dcc827338834b8ed9333622c2
Kevin Baker <eire32kevin@gmail.com>
2014-02-21 07:52:48 +0000

    Added tests for the university pages

--- project/teams/tests.py
+++ project/teams/tests.py
@@ -8,83 +8,83 @@ from teams.models import Team, Checkin, world_distance
 from utilities.utils import CleanTestCase
 
 class TeamModelTest(CleanTestCase):
-	'''
-	Tests the methods of the Team model class
+    '''
+    Tests the methods of the Team model class
 
-	Team 1 has multiple checkins
-	Team 2 has one checkin
-	Team 3 has no checkins
-	'''
+    Team 1 has multiple checkins
+    Team 2 has one checkin
+    Team 3 has no checkins
+    '''
 
-	fixtures = [
-		'teams.json',
-		'checkins.json'
-	]
+    fixtures = [
+        'teams.json',
+        'checkins.json'
+    ]
 
-	def test_last_checkin(self):
-		'''Test Team.last_checkin()'''
-		team1 = Team.objects.get(pk=1)
-		self.assertEqual(team1.last_checkin.pk, 8)
+    def test_last_checkin(self):
+        '''Test Team.last_checkin()'''
+        team1 = Team.objects.get(pk=1)
+        self.assertEqual(team1.last_checkin.pk, 8)
 
-		team3 = Team.objects.get(pk=3)
-		self.assertIsNone(team3.last_checkin)
+        team3 = Team.objects.get(pk=3)
+        self.assertIsNone(team3.last_checkin)
 
-	def test_world_distance(self):
-		'''Tests teams.models.world_distance() a common
-		function for calculating the distance between two
-		GPS coordinates.
-		'''
-		dublin_lat = radians(Decimal("53.3418"))
-		dublin_lng = radians(Decimal("-6.3098"))
+    def test_world_distance(self):
+        '''Tests teams.models.world_distance() a common
+        function for calculating the distance between two
+        GPS coordinates.
+        '''
+        dublin_lat = radians(Decimal("53.3418"))
+        dublin_lng = radians(Decimal("-6.3098"))
 
-		galway_lat = radians(Decimal("53.2741"))
-		galway_lng = radians(Decimal("-9.0498"))
+        galway_lat = radians(Decimal("53.2741"))
+        galway_lng = radians(Decimal("-9.0498"))
 
-		# same point twice
-		result = world_distance(dublin_lat, dublin_lng, dublin_lat, dublin_lng)
-		self.assertEqual(result, 0)
+        # same point twice
+        result = world_distance(dublin_lat, dublin_lng, dublin_lat, dublin_lng)
+        self.assertEqual(result, 0)
 
-		# Dublin to Galway
-		result = world_distance(dublin_lat, dublin_lng, galway_lat, galway_lng)
-		self.assertAlmostEqual(182.248, result, places=2)
+        # Dublin to Galway
+        result = world_distance(dublin_lat, dublin_lng, galway_lat, galway_lng)
+        self.assertAlmostEqual(182.248, result, places=2)
 
-	def test_distance(self):
-		'''Tests Team.distance property on Team object'''
-		team1 = Team.objects.get(pk=1)
-		self.assertAlmostEqual(5917, team1.distance, places=0)
+    def test_distance(self):
+        '''Tests Team.distance property on Team object'''
+        team1 = Team.objects.get(pk=1)
+        self.assertAlmostEqual(5917, team1.distance, places=0)
 
-		team2 = Team.objects.get(pk=2)
-		self.assertAlmostEqual(464, team2.distance, places=0)
+        team2 = Team.objects.get(pk=2)
+        self.assertAlmostEqual(464, team2.distance, places=0)
 
-		team3 = Team.objects.get(pk=3)
-		self.assertEqual(0, team3.distance)
+        team3 = Team.objects.get(pk=3)
+        self.assertEqual(0, team3.distance)
 
-	def test_distance_travelled(self):
-		'''Tests Team.distance_travelled property on Team object'''
-		team1 = Team.objects.get(pk=1)
-		self.assertAlmostEqual(6100, team1.distance_travelled, places=0)
+    def test_distance_travelled(self):
+        '''Tests Team.distance_travelled property on Team object'''
+        team1 = Team.objects.get(pk=1)
+        self.assertAlmostEqual(6100, team1.distance_travelled, places=0)
 
-		team2 = Team.objects.get(pk=2)
-		self.assertAlmostEqual(464, team2.distance_travelled, places=0)
+        team2 = Team.objects.get(pk=2)
+        self.assertAlmostEqual(464, team2.distance_travelled, places=0)
 
-		team3 = Team.objects.get(pk=3)
-		self.assertEqual(0, team3.distance_travelled)
+        team3 = Team.objects.get(pk=3)
+        self.assertEqual(0, team3.distance_travelled)
 
-	def test_unicode(self):
-		'''Test the __unicode__ method on Team'''
-		team = Team.objects.get(pk=1)
-		self.assertIsNotNone(unicode(team))
+    def test_unicode(self):
+        '''Test the __unicode__ method on Team'''
+        team = Team.objects.get(pk=1)
+        self.assertIsNotNone(unicode(team))
 
 class CheckinModelTest(CleanTestCase):
-	fixtures = [
-		'teams.json',
-		'checkins.json'
-	]
+    fixtures = [
+        'teams.json',
+        'checkins.json'
+    ]
 
-	def test_unicode(self):
-		'''Test the __unicode__ method on Checkin'''
-		checkin = Checkin.objects.get(pk=1)
-		self.assertIsNotNone(unicode(checkin))
+    def test_unicode(self):
+        '''Test the __unicode__ method on Checkin'''
+        checkin = Checkin.objects.get(pk=1)
+        self.assertIsNotNone(unicode(checkin))
 
 class TeamViewTest(CleanTestCase):
     '''Tests the team profile view page'''
@@ -121,3 +121,40 @@ class TeamsViewTest(CleanTestCase):
 
         # test the returned context makes some sense
         self.assertIn('teams', resp.context)
+
+class UniversitiesViewTest(CleanTestCase):
+    '''This code is a clusterf*ck just well badly test it'''
+    fixtures = [
+        'teams.json',
+        'checkins.json'
+    ]
+
+    def test_200(self):
+        '''Tests that when we request Universities that exists we get a 200 response'''
+        resp = self.client.get(reverse('universities'))
+        self.assertEqual(resp.status_code, 200)
+
+        # test the returned context makes some sense
+        self.assertIn('universities', resp.context)
+        self.assertEqual(resp.context['best_travellers_distance'], 3190)
+        self.assertEqual(resp.context['best_raisers_amount'], 3321)
+
+class UniversityViewTest(CleanTestCase):
+    fixtures = [
+        'teams.json',
+        'checkins.json'
+    ]
+
+    def test_200(self):
+        '''Tests that when we request a University that exists we get a 200 response'''
+        resp = self.client.get(reverse('university', args=['tcd']))
+        self.assertEqual(resp.status_code, 200)
+
+        # test the returned context makes some sense
+        self.assertIn('university', resp.context)
+
+    def test_404(self):
+        '''Tests that if we lookup a bullshit university we return a 404'''
+        resp = self.client.get(reverse('university', args=['nope']))
+        self.assertEqual(resp.status_code, 404)
+
